cb63411b57f10c0e96fa203c86609aab7ac47a43
Anurag Bhandari <anuragbhandari77736@gmail.com>
2023-12-26 10:25:20 +0530

    Progress saved

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -26,7 +26,7 @@ export async function POST(req: Request) {
       status: 400
     })
   }
- 
+ //eee
   // Get the body
   const payload = await req.json()
   const body = JSON.stringify(payload);
